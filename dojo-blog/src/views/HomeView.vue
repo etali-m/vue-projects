9848f83298da197da2cedb05db95c2f3337fe360
etali-m <etalijunior15@gmail.com>
2025-02-06 11:55:20 +0100

    blog avec l'api compositin

---
+++ dojo-blog/src/views/HomeView.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="home"> 
+    <div v-if="error">{{ error }}</div>
+    <div v-if="posts.length">
+      <PostList :posts="posts"/>
+    </div>
+    <div v-else>Loading....</div>
+  </div>
+</template> 
+
+<script> 
+import { ref } from 'vue'
+import PostList from '../components/PostList.vue'
+import getPosts from '../composables/getPosts'
+
+export default {
+  name: 'HomeView', 
+  components: { PostList },
+  setup() {
+
+    const { posts, error, load } = getPosts() //récupère les valeur retournées par la fonction getPosts
+
+    load()
+    
+    return { posts, error }
+  } 
+}
+</script>
